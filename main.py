a49a099287f50ec7bda8b54af3a15b1724dc2845
Вячеслав Лукин <bafana.slv@gmail.com>
2024-05-17 14:29:17 +0300

    Исправление замечаний.

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ import os
 
 EMPLOYERS_FILE = ROOT_DIR+'/employers_file.json'  # файл с списком выбранных работодателей.
 URL_GET = "https://api.hh.ru/vacancies"  # адрес HeadHanter для отправки запроса.
-SEARCH_WORD = "oracle"  # поисковое слово для запроса на HeadHanter.
-COUNT_EMPLOYERS = 10  # количество интересующих работодателей.
 SEARCH_CONFIGURATION_FILE = ROOT_DIR+'/search_parameters.ini'  # конфигурационный файл для параметров поиска.
 
 
@@ -28,21 +26,23 @@ def users_menu():
     config = configparser.ConfigParser()
     config.read(SEARCH_CONFIGURATION_FILE)
     search_config = dict(config.items('search'))
+    search_word = search_config["employers_count"]           # поисковое слово для запроса на HeadHanter
+    count_employers = int(search_config["employers_count"])  # количество интересующих работодателей
     print(f'{search_config["employers_count"]} интересующих работодателей будут выбраны по ключевому слову '
           f'"{search_config["search_word"]}".')
     page_quantity = input(f'Введите количество страниц (не более 20):\n')
     # параметры поиска на HeadHanter
-    params = {'text': SEARCH_WORD, 'area': '113', 'currency': 'RUR', 'per_page': 100, 'page': 0}
+    params = {'text': search_word, 'area': '113', 'currency': 'RUR', 'per_page': 100, 'page': 0}
     if not page_quantity.isdigit() or int(page_quantity) > 20:
         print('Неверный ввод - по умолчанию будет выбраны 2 страницы!')
         page_quantity = '2'
     if not os.path.isfile(EMPLOYERS_FILE):
         # в случае отсуствия файла с работодателями он формируется по запросу к HeadHanter из вакансий.
-        # read_employers_list - функция для формирования списка работодателей selected_employers
+        # read_employers_list - функция для формирования списка работодателей.
         selected_employers = read_employers_lists(params, int(page_quantity), URL_GET)
         if len(selected_employers) > 0:
-            # в окончательный список после сортировки берем только COUNT_EMPLOYERS вакансий с наивысшей зарплатой.
-            selected_emp_10 = sorted(selected_employers, key=lambda x: x[3], reverse=True)[0:COUNT_EMPLOYERS]
+            # в окончательный список после сортировки берем только count_employers вакансий с наивысшей зарплатой.
+            selected_emp_10 = sorted(selected_employers, key=lambda x: x[3], reverse=True)[0:count_employers]
             # сохраняем работодателей в json-файле.
             save_json_file(selected_emp_10, EMPLOYERS_FILE)
             # на базе полученных работодателей формируем соовествующий им список вакансий.
@@ -53,7 +53,7 @@ def users_menu():
     else:
         # в случае если json-файл с работодателями уже есть, то загружаем их из файла.
         selected_emp_10 = load_json_file(EMPLOYERS_FILE)
-        # на базе загруженных работодателей формируем соовествующий им список вакансий.
+        # на базе загруженных работодателей формируем сооветствующий им список вакансий.
         selected_vac = read_vacancies_lists(params, int(page_quantity), selected_emp_10, URL_GET)
         database_manager(selected_emp_10, selected_vac)
 
